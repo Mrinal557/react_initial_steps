043ea8eb86863a95da3f18332180561c970eb680
Mrinal Anand <mrinal.annand@gmail.com>
2023-01-12 23:25:37 +0530

    it's the basics of reactjs

---
+++ myapp/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>Thapa Technical</h1>
+        <p>Plz like and share</p>
+        <h2>Plz subscribe my channel</h2>
+    </div>,
+    document.getElementById("root")
+);
