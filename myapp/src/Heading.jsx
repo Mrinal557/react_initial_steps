bad5f767bbf57098d289463e687ddd59a51143f6
Mrinal Anand <mrinal.annand@gmail.com>
2023-01-14 13:10:51 +0530

    initial commit

---
+++ myapp/src/Heading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Heading()
+{
+    return <h1>My name is Mrinal</h1>;
+}
+export default Heading;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,11 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
+// const myname = "mrinal";
+
+let curDate = new Date();
+curDate = curDate.getHours();
+
+let greeting = '';
+const cssStyle = {
+    color: 'green'
+}
+if (curDate >= 1 && curDate < 12)
+{
+    greeting = 'Good Morning';
+    cssStyle.color = 'Green';
+}
+else if (curDate >= 12 && curDate < 19)
+{ 
+    greeting = 'Good Afternoon';
+    cssStyle.color = 'Orange';
+}
+else
+{
+    greeting = 'Good Night';
+    cssStyle.color = 'Black';
+}
 
 ReactDOM.render(
-    <div>
-        <h1>Thapa Technical</h1>
-        <p>Plz like and share</p>
-        <h2>Plz subscribe my channel</h2>
-    </div>,
-    document.getElementById("root")
+    <>
+        <div>
+            <h1>Hello sir, <span style={cssStyle}>{greeting}</span>
+            </h1>
+            </div>
+        </>,
+    document.getElementById('root')
 );
