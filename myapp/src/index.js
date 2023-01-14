74140ecb224f807c5bfe2712ba02365ed4ffcbee
Mrinal Anand <mrinal.annand@gmail.com>
2023-01-14 13:20:48 +0530

    added new files

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,37 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css';
+// import './index.css';
 // const myname = "mrinal";
+import Heading from "./Heading";
+import Para from "./Para";
 
-let curDate = new Date();
-curDate = curDate.getHours();
+// let curDate = new Date();
+// curDate = curDate.getHours();
 
-let greeting = '';
-const cssStyle = {
-    color: 'green'
-}
-if (curDate >= 1 && curDate < 12)
-{
-    greeting = 'Good Morning';
-    cssStyle.color = 'Green';
-}
-else if (curDate >= 12 && curDate < 19)
-{ 
-    greeting = 'Good Afternoon';
-    cssStyle.color = 'Orange';
-}
-else
-{
-    greeting = 'Good Night';
-    cssStyle.color = 'Black';
-}
+// let greeting = '';
+// const cssStyle = {
+//     color: 'green'
+// }
+// if (curDate >= 1 && curDate < 12)
+// {
+//     greeting = 'Good Morning';
+//     cssStyle.color = 'Green';
+// }
+// else if (curDate >= 12 && curDate < 19)
+// { 
+//     greeting = 'Good Afternoon';
+//     cssStyle.color = 'Orange';
+// }
+// else
+// {
+//     greeting = 'Good Night';
+//     cssStyle.color = 'Black';
+// }
 
 ReactDOM.render(
     <>
-        <div>
-            <h1>Hello sir, <span style={cssStyle}>{greeting}</span>
-            </h1>
-            </div>
+        <Heading>
+        <Para/>
+            <p>This is my world!!</p>
+            <ol>
+                <li>Mrinal</li>
+                <li>Anand</li>
+                <li>NIT Agartala</li>
+            </ol>
+        </Heading>
         </>,
     document.getElementById('root')
 );
