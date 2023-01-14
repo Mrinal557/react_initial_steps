e158fcdfaee943c2a65903392731789ad99b57c5
Mrinal Anand <mrinal.annand@gmail.com>
2023-01-14 14:41:34 +0530

    added App.jsx

---
+++ myapp/src/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+function List()
+{
+    return (
+        <ol>
+            <li>Mrinal</li>
+            <li>Anand</li>
+            <li>NIT Agartala</li>
+            <li>How is it working!!</li>
+        </ol>
+    );
+}
+export default List;
